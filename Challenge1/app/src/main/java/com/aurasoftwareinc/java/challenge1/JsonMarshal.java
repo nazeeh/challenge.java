e147ebd1391b28261ef025b99fdfcf2ae3d7051d
nazeeh <nazeeh@Nazeehs-MBP-2.home>
2018-12-05 14:59:10 +0100

    Code organization and formatting

--- Challenge1/app/src/main/java/com/aurasoftwareinc/java/challenge1/JsonMarshal.java
+++ Challenge1/app/src/main/java/com/aurasoftwareinc/java/challenge1/JsonMarshal.java
@@ -16,29 +16,21 @@ import static android.content.ContentValues.TAG;
 
 public class JsonMarshal
 {
+    //This hash set includes the boxed types which can be directly added to the json object when found
     private static final Set<Class> WRAPPER_TYPES = new HashSet(Arrays.asList(
             Boolean.class, Character.class, Byte.class, Short.class, Integer.class, Long.class, Float.class, Double.class, Void.class));
 
+    //Loop over the members of the class (make any private members accessible)
+    //If the member object is primitive, store it in the json object
+    //If the member object is not primitive but implements the JsonMarshal interface, inspect it (recursively)
+    //If the member object is not primitive and does not implement the interface, store it in the json file as string
     public static JSONObject marshalJSON(Object object)
     {
         JSONObject json = new JSONObject();
 
         //Remove the package name from the name of the key for easier readability
         String objectKeyTitle = object.getClass().getName().substring(object.getClass().getName().lastIndexOf('.') + 1);
-
-        try {
-            json.put(objectKeyTitle, InspectObject(object));
-        }
-        catch (JSONException exc) {
-
-        }
-
-
-        //Loop over the members of the class (make any private members accessible)
-        //If the member variable is primitive, store it in the json object
-        //If the member variable is not primitive but implements the JsonMarshal interface, inspect it (recursively)
-        //If the member variable is not primitive and does not implement the interface, store it in the json file as string
-        //Remember to check for serialVersionUID and not to include it in the Json object
+        addObjectToJSON(json, objectKeyTitle, InspectObject(object));
 
         return json;
     }
@@ -62,50 +54,28 @@ public class JsonMarshal
                     //Recursion in case the member object implements JsonMarshalInterface
                     if (memberAsObject instanceof JsonMarshalInterface) {
                         String objectKeyTitle = memberAsObject.getClass().getName().substring(memberAsObject.getClass().getName().lastIndexOf('.') + 1);
-                        try {
-                            json.put(objectKeyTitle, InspectObject(memberAsObject));
-                        }
-                        catch (JSONException exc) {
-
-                        }
+                        addObjectToJSON(json, objectKeyTitle, InspectObject(memberAsObject));
                     }
                     //Check for JSONObjects, JSONArrays and wrapped primitive types add them to the resulting json object
                     //Ignore synthetic members (the compiler generates a member called $change which is generated at runtime)
                     else if (!parentObjectMembers[i].isSynthetic()){
                         if ((memberAsObject instanceof JSONObject) || (memberAsObject instanceof JSONArray) || (WRAPPER_TYPES.contains(parentObjectMembers[i].getType()) && parentObjectMembers[i].get(object)!=null)) {
-                            try {
-                                json.put(parentObjectMembers[i].getName(), parentObjectMembers[i].get(object));
-                            }
-                            catch (JSONException exc) {
-
-                            }
+                            addObjectToJSON(json, parentObjectMembers[i].getName(), parentObjectMembers[i].get(object));
                         }
                         //Check for byte arrays
                         else if  (memberAsObject instanceof byte[]) {
-                            try {
-                                json.put(parentObjectMembers[i].getName(), new String((byte[]) parentObjectMembers[i].get(object)));
-                            }
-                            catch (JSONException exc) {
-                                Log.d("Something went wrong.. ", exc.getMessage());
-                            }
+                            addObjectToJSON(json, parentObjectMembers[i].getName(), new String((byte[]) parentObjectMembers[i].get(object)));
                         }
                         else {
                             //If the object is not primitive nor wrapped primitive and does not implement the JSonMarshal interface, then show it as a string of its name
-                            try {
-                                json.put(parentObjectMembers[i].getName(), parentObjectMembers[i].getName());
-                            } catch (JSONException exception) {
-                            }
+                            addObjectToJSON(json, parentObjectMembers[i].getName(), parentObjectMembers[i].getName());
                         }
                     }
                 }
                 //Store primitive objects in the json object directly
                 //Do not include the object whose name is serialVersionUID since it's generated at runtime
                 else if (parentObjectMembers[i].getName() != "serialVersionUID") {
-                    try {
-                        json.put(parentObjectMembers[i].getName(), parentObjectMembers[i].get(object).toString());
-                    }
-                    catch (JSONException exception) {
-                    }
+                    addObjectToJSON(json, parentObjectMembers[i].getName(), parentObjectMembers[i].get(object).toString());
                 }
             }
             catch (IllegalAccessException exception) {
@@ -113,6 +83,18 @@ public class JsonMarshal
         }
 
         return json;
+    }
+
+    //I'm using this function to make the code more readable
+    private static JSONObject addObjectToJSON(JSONObject jsonObject, String key, Object value) {
+        try {
+            jsonObject.put(key, value);
+        }
+        catch (JSONException exception) {
+            Log.e("Json exception", "Error while adding item " + key + " to the json object: " + exception.getMessage());
+        }
+
+        return jsonObject;
     }
 
     public static boolean unmarshalJSON(Object object, JSONObject json)
